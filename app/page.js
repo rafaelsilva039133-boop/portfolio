45cd9179d5368e8ed21b012a2b38091925b0f90b
Rafael Silva <rafaelsilva039133@gmail.com>
2026-02-20 17:13:03 -0300

    feat: all pages in page.js

--- app/page.js
+++ app/page.js
@@ -1,3 +1,7 @@
+import About from "@/components/about";
+import Contact from "@/components/contact";
+import Services from "@/components/services";
+import Header from "@/components/header";
 import Hero from "@/components/hero";
 import Projects from "@/components/projects";
 
@@ -5,8 +9,12 @@ export default function Home() {
  
   return (
     <main>
+      <Header/>
       <Hero/>
       <Projects/>
+      <About/>
+      <Services/>
+      <Contact/>
     </main>
   );
 }
