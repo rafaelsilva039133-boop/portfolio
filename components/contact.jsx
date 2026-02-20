09452a19afa50ae46ec11dcc969d599e6c30b823
Rafael Silva <rafaelsilva039133@gmail.com>
2026-02-20 16:55:02 -0300

    contact page

---
+++ components/contact.jsx
@@ -0,0 +1,10 @@
+
+function Contact(){ 
+  return(
+    <section id="contact" className="fullscreen flex items-center justify-center">
+      <h1>Contato</h1>
+    </section>
+  )
+}
+
+export default Contact;
