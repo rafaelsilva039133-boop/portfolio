b1334aa6399bfd67df1afdf036c83214ff8274e9
Rafael Silva <rafaelsilva039133@gmail.com>
2026-02-20 16:50:58 -0300

    Services

---
+++ components/services.jsx
@@ -0,0 +1,10 @@
+
+function Services(){ 
+  return(
+    <section id="services" className="fullscreen flex items-center justify-center">
+      <h1>Serviços</h1>
+    </section>
+  )
+}
+
+export default Services;
