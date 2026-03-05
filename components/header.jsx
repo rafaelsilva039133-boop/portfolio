f42b0faad76e89cfddf332c4009bedaacc2c0786
Rafael Silva <rafaelsilva039133@gmail.com>
2026-03-04 23:27:40 -0300

    fix: strange transition

--- components/header.jsx
+++ components/header.jsx
@@ -1,33 +1,53 @@
 'use client'
 
-import Image from "next/image";
 import { useState } from "react";
 import Link from "next/link";
 
 function Header(){ 
 
   const [menu, setMenu] = useState(false);
+  const [showLinks, setShowLinks] = useState(false)
+
+  function toggleMenu() {
+  if (!menu) {
+    setMenu(true)
+
+    setTimeout(() => {
+      setShowLinks(true)
+    }, 300) 
+  } else {
+    setShowLinks(false)
+
+    setTimeout(() => {
+      setMenu(false)
+    }, 300)
+  }
+}
 
   return(
     <header className="fixed top-0 left-0 w-full bg-red-500 shadow-md z-20 ">
 
       <button 
         className="absolute top-5.5 right-10 md:right-30"
-        onClick={() => setMenu(!menu)}>
+        onClick={() => toggleMenu()}>
         menu
       </button>
 
       <div className={`
         flex justify-between items-center
-        transition-all duration-600 
-        ${menu ? "h-screen px-120" : "h-18 px-30"}
+        transition-[height] duration-500 
+        ${menu ? "h-screen px-20 sm:px-30 md:px-40 lg:px-60 xl:px-90 2xl:px-110" : "h-18 px-30"}
       `}>
 
-        <Image src={"/logo.svg"} width={50} height={50} alt="logo" />
+        <img src={"/logo.svg"} alt="logo" className={` transition-all duration-500 ${menu ? " h-30 sm:h-40 md:h-50" : "h-14"}`} />
 
         {
           menu 
-          ?(<div className="gap-6 pr-20 flex flex-col">
+          ?(<div   className={`
+            gap-6 flex flex-col
+            transition-all duration-500 
+            ${showLinks ? "opacity-100 translate-y-0" : "opacity-0 translate-y-4"}
+          `}>
           <Link href={"/#home"}>Inicio</Link>
           <Link href={"/#projects"}>Projetos</Link>
           <Link href={"/#about"}>Sobre</Link>
@@ -35,7 +55,7 @@ function Header(){
           <Link href={"/#plans"}>Planos</Link>
           <Link href={"/#contact"}>Contato</Link>
         </div>)
-        :(<div className="gap-6 pr-20 flex">
+        :(<div className="gap-6 md:pr-20 flex">
           <Link href={"/#home"}>Inicio</Link>
           <Link href={"/#projects"}>Projetos</Link>
           <Link href={"/#about"}>Sobre</Link>
