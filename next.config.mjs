fae80a70f6ce0773bd0214d17ccee741d7b7a1cb
Rafael Silva <rafaelsilva039133@gmail.com>
2026-03-05 21:46:15 -0300

    chore: configure i18n

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
+import createNextIntlPlugin from 'next-intl/plugin';
+
 const nextConfig = {
   /* config options here */
 };
 
-export default nextConfig;
+const withNextIntl = createNextIntlPlugin();
+export default withNextIntl(nextConfig);
