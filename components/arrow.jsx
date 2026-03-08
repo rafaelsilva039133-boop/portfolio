6f2837f626657c288cf2af66c760af9fc0a271e8
Rafael Silva <rafaelsilva039133@gmail.com>
2026-03-07 22:07:09 -0300

    feat: component arrow

---
+++ components/arrow.jsx
@@ -0,0 +1,9 @@
+function Arrow() {
+  return (
+    <svg viewBox="0 0 24 24" className="w-6 h-6 fill-current text-(--color-assistant)">
+      <path d="M5 12h14M13 5l7 7-7 7" />
+    </svg>
+  )
+}
+
+export default Arrow;
