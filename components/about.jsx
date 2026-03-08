75ddc85ed68e05568b910b21605b6c9d3e346bb7
Rafael Silva <rafaelsilva039133@gmail.com>
2026-03-08 15:28:11 -0300

    style: About page

--- components/about.jsx
+++ components/about.jsx
@@ -4,9 +4,11 @@ function About(){
   const t = useTranslations("About");
 
   return(
-    <section id="about" className="fullscreen flex items-center justify-center">
-      <h1>{t("title")}</h1>
-      <p>{t("description")}</p>
+    <section id="about" className="fullscreen flex flex-col justify-center responsive-container">
+      <div className=" w-3/5">
+        <h1 className=" title pb-4">{t("title")}</h1>
+        <p className=" text">{t("description")}</p>
+      </div>
     </section>
   )
 }
