4c66d158be2c11504c0140f88acdbddce40c4730
Rafael Silva <rafaelsilva039133@email.com>
2026-04-29 17:02:53 -0300

    refactor: contact page

--- components/contact.jsx
+++ components/contact.jsx
@@ -8,16 +8,16 @@ function Contact(){
       <div className="flex flex-col gap-2 pb-6 md:pb-0">
         <h1 className="title">{t("title")}</h1>
         <p className="md:pb-2 text text-(--color-text)">{t("description")}</p>
-        <a className="text text-(--color-assistant) font-bold" href="mailto:rafaelsilva039133@gmail.com" target="_blank" rel="noopener noreferrer"> <span className="border-text hover:border-zinc-700 hover:text-zinc-700">rafaelsilva039133@gmail.com</span> </a>
-        <a className="text text-(--color-assistant) font-bold " href="https://wa.me/557582986745" target="_blank" rel="noopener noreferrer"> <span className="border-text hover:border-zinc-700 hover:text-zinc-700">(75) 8298-6745</span> </a>
+        <a className="text text-(--color-assistant) font-bold hover:border-zinc-700 hover:text-zinc-700 w-min border-text" href="mailto:rafaelsilva039133@gmail.com" target="_blank" rel="noopener noreferrer"> rafaelsilva039133@gmail.com</a>
+        <a className="text text-(--color-assistant) font-bold hover:border-zinc-700 hover:text-zinc-700 w-min text-nowrap border-text" href="https://wa.me/557582986745" target="_blank" rel="noopener noreferrer"> (75) 8298-6745 </a>
       </div>
       <div className="md:min-w-2/5 flex flex-col items-end">
         <div className="flex flex-col gap-1 md:gap-3">
           <h2 className="subtitle pb-2">{t("social")}</h2>
-          <a className="text text-(--color-secondary) flex gap-3 items-center" href="https://www.instagram.com/rafael.s_dev/" target="_blank" rel="noopener noreferrer"> <Arrow/> <span className="border-text hover:border-amber-950 hover:text-amber-950">Instagram</span> </a>
-          <a className="text text-(--color-secondary) flex gap-3 items-center " href="https://x.com/Rafael_S137" target="_blank" rel="noopener noreferrer"> <Arrow/> <span className="border-text hover:border-amber-950 hover:text-amber-950">X (Twitter)</span> </a>
-          <a className="text text-(--color-secondary) flex gap-3 items-center" href="https://www.linkedin.com/in/rafael-silva-7072283a4/" target="_blank" rel="noopener noreferrer"> <Arrow/> <span className="border-text hover:border-amber-950 hover:text-amber-950">Linkedin</span> </a>
-          <a className="text text-(--color-secondary) flex gap-3 items-center" href="https://wa.me/557582986745" target="_blank" rel="noopener noreferrer"> <Arrow/><span className="border-text hover:border-amber-950 hover:text-amber-950">WhatsApp</span></a>
+          <a className="text text-(--color-secondary) flex gap-3 items-center hover:text-amber-950" href="https://www.instagram.com/rafael.s_dev/" target="_blank" rel="noopener noreferrer"> <Arrow/> Instagram </a>
+          <a className="text text-(--color-secondary) flex gap-3 items-center hover:text-amber-950" href="https://x.com/Rafael_S137" target="_blank " rel="noopener noreferrer"> <Arrow/> X (Twitter) </a>
+          <a className="text text-(--color-secondary) flex gap-3 items-center hover:text-amber-950" href="https://www.linkedin.com/in/rafael-silva-7072283a4/" target="_blank" rel="noopener noreferrer"> <Arrow/> Linkedin </a>
+          <a className="text text-(--color-secondary) flex gap-3 items-center hover:text-amber-950" href="https://wa.me/557582986745" target="_blank" rel="noopener noreferrer"> <Arrow/>WhatsApp</a>
         </div>
       </div>
       
